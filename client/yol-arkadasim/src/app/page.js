3d69cdd93047b69e91350a98126acaa5d2a6a854
Deniz Ök <deniz.ok2019@gmail.com>
2024-05-04 14:30:10 +0300

    added footer and fix profile page padding

--- client/yol-arkadasim/src/app/page.js
+++ client/yol-arkadasim/src/app/page.js
@@ -1,9 +1,10 @@
 import Link from "next/link";
 import "./globals.css"
+import HomePage from "./home/page";
 export default function Home() {
   return (
     <div >
-      <Link href="/">Hello</Link>
+      <HomePage/>
     </div>
   );
 }
