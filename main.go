1e2fb43cb3002e325b7a4ade9b48b8999c49222d
Mert Sancar <msmertsancar@gmail.com>
2024-05-17 19:12:26 +0300

    update

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main() {
 <<<<<<< HEAD
+<<<<<<< HEAD
 	fmt.Println("qwerty")
 =======
 	fmt.Println("Deniz Ök: Bu araba kime çıkıyor?")
 >>>>>>> da265e4 (Merge pull request #39 from oguzhankuzlukluoglu/oguzhan)
+=======
+	fmt.Println("qwereety")
+>>>>>>> 9cdd729 (update)
 
 }
