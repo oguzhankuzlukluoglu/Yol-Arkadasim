4d7a8c8450577590fed293e8214c2d512f001588
Mert Sancar <msmertsancar@gmail.com>
2024-05-19 17:47:50 +0300

    created token.go

---
+++ client/yol-arkadasim/server/utils/token.go
@@ -0,0 +1,44 @@
+package utils
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+var jwtKey = []byte("your_secret_key")
+
+type Claims struct {
+	UserID string `json:"user_id"`
+	jwt.StandardClaims
+}
+
+func GenerateJWT(userID string) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		UserID: userID,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(jwtKey)
+}
+
+func ValidateJWT(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtKey, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, err
+	}
+
+	return claims, nil
+}
