8a88e17fbe9015d89c0af285a30bd17308608ae6
Tuğba Biçer <tbicer21@posta.pau.edu.tr>
2024-05-25 13:22:43 +0300

    added missings

--- client/yol-arkadasim/src/components/advertiseTravel/Advertise.jsx
+++ client/yol-arkadasim/src/components/advertiseTravel/Advertise.jsx
@@ -1,56 +1,119 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import { Modal, Button, Alert, Form } from "react-bootstrap";
 import styles from "./advertise.module.css";
 import Where from "../Where/Where";
-import { system } from "nodemon/lib/config";
+import DatePicker from "../datepicker/DatePicker";
+import TimePicker from "../datepicker/TimePicker";
 
 const Advertise = () => {
+  const [showAlert, setShowAlert] = useState(false);
+  const [formData, setFormData] = useState({
+    from: "",
+    to: "",
+    date: "",
+    time: "",
+    transportation: "",
+    phone: "",
+    description: "",
+  });
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
   return (
-    <div className={styles.container}>
-      <div className={styles.advertContainer}>
-        <div className={styles.advertInfos}>
-          <div className={styles.route}>
-            <div className={styles.from}>
-              <Where type="from" />
-            </div>
-            <div className={styles.to}>
-              <Where type="to" />
+    <>
+      <Button variant="primary" onClick={handleShow} className={styles.advertButton}>
+        İlan Aç
+      </Button>
+      <Modal size="xxl" show={show} onHide={handleClose} className={styles.modalContainer} dialogClassName={styles.modalDialog}>
+        <Modal.Header closeButton>
+          <Modal.Title>İlan Oluştur</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <div className={styles.container}>
+            <div className={styles.advertContainer}>
+              <div className={styles.advertInfos}>
+                <div className={styles.route}>
+                  <div className={styles.from}>
+                    <Where type="from" />
+                  </div>
+                  <div className={styles.to}>
+                    <Where type="to" />
+                  </div>
+                </div>
+                <div className={styles.time}>
+                  <div className={styles.date}>
+                    <label htmlFor="date">Tarih</label>
+                    <DatePicker/>
+                  </div>
+                  <div className={styles.clock}>
+                    <label htmlFor="time">Saat</label>
+                    <TimePicker/>
+                  </div>
+                </div>
+                <div className={styles.information}>
+                  <div className={styles.transport}>
+                    <label htmlFor="transportation">Ulaşım yolu</label>
+                    <input
+                      type="text"
+                      id="transportation"
+                      name="transportation"
+                      placeholder="araba vs."
+                      value={formData.transportation}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                  <div className={styles.tel}>
+                    <label htmlFor="phone">Telefon</label>
+                    <input
+                      type="tel"
+                      id="phone"
+                      name="phone"
+                      value={formData.phone}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.routeInfos}>
+                <div className={styles.desc}>
+                  <div className={styles.routeDesc}>
+                    <label htmlFor="description">Açıklama</label>
+                    <textarea
+                      id="description"
+                      name="description"
+                      cols="30"
+                      rows="8"
+                      value={formData.description}
+                      onChange={handleInputChange}
+                    ></textarea>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-          <div className={styles.time}>
-            <div className={styles.date}>
-              <label htmlFor="tarih">Tarih</label>
-              <input type="date" id="tarih" />
-            </div>
-            <div className={styles.clock}>
-              <label htmlFor="saat">Saat</label>
-              <input type="time" id="saat" />
-            </div>
-          </div>
-          <div className={styles.information}>
-            <div className={styles.transport}>
-              <label htmlFor="ulaşım">Ulaşım yolu</label>
-              <input type="text" id="ulaşım" placeholder="araba vs." />
-            </div>
-            <div className={styles.tel}>
-              <label htmlFor="telefon">Telefon</label>
-              <input type="tel" id="telefon" />
-            </div>
-          </div>
-        </div>
-        <div className={styles.routeInfos}>
-          <div className={styles.desc}>
-            <div className={styles.routeDesc}>
-              <label htmlFor="acıklama">Açıklama</label>
-              <textarea id="acıklama" cols="30" rows="8"></textarea>
-            </div>
-            <div className={styles.advertButton}>
-              <button>İlanı Aç</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Kapat
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Kaydet
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
   );
 };
 
-export default Advertise;
+export default Advertise;
